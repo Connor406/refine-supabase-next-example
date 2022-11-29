c2e4e81b92dbbd8be6c87a59155cf7f40c80652b
Connor Mahan <connor@reachcreative.com>
2022-11-29 11:18:52 -0600

    initial commit with superplate

---
+++ pages/[[...refine]].tsx
@@ -0,0 +1,35 @@
+import { GetServerSideProps } from "next";
+import {
+  NextRouteComponent /*, handleRefineParams */,
+} from "@pankod/refine-nextjs-router";
+import { checkAuthentication } from "@pankod/refine-nextjs-router";
+
+import { authProvider } from "src/authProvider";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // const { resource, action, id } = handleRefineParams(context.params?.refine);
+
+  const { isAuthenticated, ...props } = await checkAuthentication(
+    authProvider,
+    context
+  );
+
+  if (!isAuthenticated) {
+    return props;
+  }
+
+  return {
+    props: {},
+  };
+};
+
+export default NextRouteComponent;
+
+/**
+ * To define a custom initial route for refine to redirect and start with:
+ *
+ * Bind the `initialRoute` value to the `NextRouteComponent` like the following:
+ *
+ * export default NextRouteComponent.bind({ initialRoute: "/posts" });
+ *
+ **/
---
+++ pages/_app.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { Refine } from "@pankod/refine-core";
+import {
+  AuthPage,
+  notificationProvider,
+  ChakraProvider,
+  refineTheme,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-chakra-ui";
+import routerProvider from "@pankod/refine-nextjs-router";
+import { dataProvider } from "@pankod/refine-supabase";
+import { authProvider } from "src/authProvider";
+import { supabaseClient } from "src/utility";
+import { Title, Sider, Layout, Header } from "@components/layout";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider theme={refineTheme}>
+      <Refine
+        routerProvider={routerProvider}
+        dataProvider={dataProvider(supabaseClient)}
+        authProvider={authProvider}
+        LoginPage={AuthPage}
+        notificationProvider={notificationProvider()}
+        ReadyPage={ReadyPage}
+        catchAll={<ErrorComponent />}
+        Title={Title}
+        Sider={Sider}
+        Layout={Layout}
+        Header={Header}
+      >
+        <Component {...pageProps} />
+      </Refine>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
